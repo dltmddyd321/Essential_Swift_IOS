27029ed2072f9cc37f0608abbf2d13eda4d8cf1c
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-07-24 14:44:47 -0700

    2021_07_24
    CollectionView(Grid), Animation

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BountyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
     
     //MVVM
     
@@ -52,28 +52,66 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         // Do any additional setup after loading the view.
     }
     
-    
-    //UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return bountyList.count
+    // UICollectionViewDataSource
+    //몇 개의 아이템을 보여줄 것인가?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
-    } //몇 개의 셀이 활성화 될 것인지 지정 -> 리스트 요소 개수 만큼 활성
+    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
+    //셀은 어떤 방식으로 표현할 것인가?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
         }
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
+        
         cell.update(info: bountyInfo)
         return cell
-    } //등록하여 반환할 셀의 id 지정
+    }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("--> \(indexPath.row)")
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
-    } //세그웨이 지정
-    //sender에 index.row를 지정함으로서 몇 번째 셀인지를 전송
+    // UICollectionViewDelegate
+    //셀이 클릭되었을때 어떻게 진행할 것인가?
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("--> \(indexPath.item)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+    }
+    
+    //UICollectionViewDelegateFlowLayout
+    //셀 사이즈 계산 -> 각종 디바이스에서 일관적인 형태를 보여주기 위함.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10
+        let textAreaHeight: CGFloat = 65
+        
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2
+        //bounds로 collectionView의 크기를 알 수 있음.
+        let height: CGFloat = width * 10/7 + textAreaHeight
+        return CGSize(width: width, height: height)
+    }
+    
+    
+//    //UITableViewDataSource
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        return bountyList.count
+//        return viewModel.numOfBountyInfoList
+//    } //몇 개의 셀이 활성화 될 것인지 지정 -> 리스트 요소 개수 만큼 활성
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+//        cell.update(info: bountyInfo)
+//        return cell
+//    } //등록하여 반환할 셀의 id 지정
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("--> \(indexPath.row)")
+//        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+//    } //세그웨이 지정
+//    //sender에 index.row를 지정함으로서 몇 번째 셀인지를 전송
 }
 
 class ListCell: UITableViewCell { //커스텀 뷰 생성
@@ -87,6 +125,9 @@ class ListCell: UITableViewCell { //커스텀 뷰 생성
         bountyLabel.text = "\(info.bounty)"
     }
 }
+
+
+
 
 
 //코드 구조화에 대한 전력 -> 기술 부채 최소화 / 재사용 및 지속 가능
@@ -124,5 +165,17 @@ class BountyViewModel {
     
     func bountyInfo(at index: Int) -> BountyInfo {
         return sortedList[index]
+    }
+}
+
+class GridCell: UICollectionViewCell { //커스텀 뷰 생성
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+    func update(info: BountyInfo) {
+        imgView.image = info.image
+        nameLabel.text = info.name
+        bountyLabel.text = "\(info.bounty)"
     }
 }
--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -24,12 +24,61 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+        
+    }
+    
+    private func prepareAnimation() {
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    
+    private func showAnimation() {
+//        nameLabelCenterX.constant = 0
+//        bountyLabelCenterX.constant = 0
+//
+//        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+//        //텍스트에 대한 애니메이션 효과
+//
+//        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+//        //이미지에 대한 애니메이션 효과
+//
+//        UIView.animate(withDuration: 0.3) {
+//            self.view.layoutIfNeeded()
+//        } //0.3초 동안 애니메이션 효과 적용
+        
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        } //0.3초 동안 애니메이션 효과 적용
     }
     
     func updateUI() {
